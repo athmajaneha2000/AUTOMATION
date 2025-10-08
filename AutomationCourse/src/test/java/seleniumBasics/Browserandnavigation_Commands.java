8c6486dec8041e6790a48807104bc9604b0c1c82
Admin <Admin@DESKTOP-IJLMRJB>
2025-10-08 14:28:45 +0530

    My commit

--- AutomationCourse/src/test/java/seleniumBasics/Browserandnavigation_Commands.java
+++ AutomationCourse/src/test/java/seleniumBasics/Browserandnavigation_Commands.java
@@ -8,16 +8,28 @@ public class Browserandnavigation_Commands extends Base
 	   System.out.println(title);
 	   
 	   String source = driver.getPageSource();
-	   System.out.println(source);
+	 //  System.out.println(source);
 	   
+	   String url = driver.getCurrentUrl();
+	   System.out.println(url);
 	   
+	   String handleid = driver.getWindowHandle();
+	   System.out.println(handleid);
+	}
+	public void navigationCommand()
+	{
+		driver.navigate().to("https://www.amazon.in/");
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
 	}
 
 	public static void main(String[] args)
 	{
 		Browserandnavigation_Commands commands = new Browserandnavigation_Commands();
 		commands.initialiseBrowser();
-		commands.browserCommand();
+		//commands.browserCommand();
+		commands.navigationCommand();
 		
 		// TODO Auto-generated method stub
 	}
