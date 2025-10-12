b22bfaf9c304e8403d80632f96cf5e5b7e90c09b
Admin <Admin@DESKTOP-IJLMRJB>
2025-10-13 05:16:03 +0530

    My commit

--- AutomationCourse/src/test/java/seleniumBasics/Locator.java
+++ AutomationCourse/src/test/java/seleniumBasics/Locator.java
@@ -13,14 +13,17 @@ public void verifyMethods()
 	driver.findElement(By.linkText("Simple Form Demo"));
 	driver.findElement(By.partialLinkText("Simple"));
 	driver.findElement(By.cssSelector("button[id='button-one']"));
-	driver.findElement(By.xpath("//button[@id='button-two']"));
+	driver.findElement(By.xpath("//button[@id='button-one']"));
 	driver.findElement(By.xpath("//button[text()=\"Show Message\"]"));
-	driver.findElement(By.xpath(" //button[@id='button-one' and @type='button']"));
-	driver.findElement(By.xpath("//button[@id='button-one' or @id='button-one-electronics']"));
+	driver.findElement(By.xpath("//button[@id='button-one' and @type='button']"));
+	driver.findElement(By.xpath(" //button[@id='button-one' or @id='button-one-electronics']"));
+	
+	
 	driver.findElement(By.xpath("//div[contains (text(), 'Single Input Field')]//parent::div[@class='card']"));
 	driver.findElement(By.xpath("//div[@class='card']//child::button[@id='button-one']"));
 	
 	 ///html/body/section/div/div/div[2]/div[1]/div/div[2]/form/button  //absolute xpath - root node
+	 
 }	
 	public static void main(String[] args) 
 	{
