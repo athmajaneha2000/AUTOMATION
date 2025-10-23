e160abc2a7375fee274e029dc6b60eb77ee49fd6
Admin <Admin@DESKTOP-IJLMRJB>
2025-10-23 12:17:00 +0530

    My commit

--- AutomationCourse/src/test/java/seleniumBasics/HandlingActions.java
+++ AutomationCourse/src/test/java/seleniumBasics/HandlingActions.java
@@ -1,5 +1,9 @@
 package seleniumBasics;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -20,12 +24,37 @@ public class HandlingActions extends Base
 	   Actions act = new Actions(driver);
 	   act.moveToElement(other).build().perform();
    }
+   public void verifyDraganddrop()
+   {
+	   driver.navigate().to(" https://demoqa.com/droppable");
+	   WebElement dragmebox = driver.findElement(By.id("draggable"));
+	   
+	   WebElement dropmebox = driver.findElement(By.id("droppable"));
+	   Actions act = new Actions(driver);
+	   act.dragAndDrop(dragmebox, dropmebox).build().perform();
+   }
+    public void verifyKeyboardaction() throws AWTException 
+    {
+    	Robot robot = new Robot();
+    	robot.keyPress(KeyEvent.VK_CONTROL);
+    	robot.keyPress(KeyEvent.VK_T);
+    	
+    	robot.keyRelease(KeyEvent.VK_CONTROL);
+    	robot.keyRelease(KeyEvent.VK_T);
+    }
 	public static void main(String[] args)
 	{
 		HandlingActions action = new HandlingActions();
 		action.initialiseBrowser();
 		//action.verifyRightclick();
-		action.verifyMousehover();
+		//action.verifyMousehover();
+		//action.verifyDraganddrop();
+		try {
+			action.verifyKeyboardaction();
+		} catch (AWTException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		// TODO Auto-generated method stub
 
 	}
---
+++ AutomationCourse/src/test/java/seleniumBasics/HandlingAlerts.java
@@ -0,0 +1,45 @@
+package seleniumBasics;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class HandlingAlerts extends Base
+{
+  public void verifySimplealert() 
+  {
+	  driver.navigate().to(" https://demoqa.com/alerts");
+	  WebElement clickbutton1 = driver.findElement(By.id("alertButton"));
+	  clickbutton1.click();
+	  Alert al = driver.switchTo().alert();
+	  al.accept();
+  }
+  public void verifyConfirmalert()
+  {
+	  driver.navigate().to(" https://demoqa.com/alerts");
+	  WebElement clickbutton3 = driver.findElement(By.id("confirmButton"));
+	  clickbutton3.click();
+	  Alert al = driver.switchTo().alert();
+	  al.accept();
+  }
+  public void verifyPromptalert()
+  {
+	  driver.navigate().to(" https://demoqa.com/alerts");
+	  WebElement clickbutton4 = driver.findElement(By.id("promtButton"));
+	  clickbutton4.click();
+	  Alert al = driver.switchTo().alert();
+	  al.sendKeys("ATHMAJA");
+	  al.accept();
+  }
+	public static void main(String[] args)
+	{
+		HandlingAlerts alert = new HandlingAlerts();
+		alert.initialiseBrowser();
+		//alert.verifySimplealert();
+		//alert.verifyConfirmalert();
+		alert.verifyPromptalert();
+		// TODO Auto-generated method stub
+
+	}
+
+}
